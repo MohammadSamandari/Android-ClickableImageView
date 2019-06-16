cff14e662213d92b36a0dec92a0962ffc7acf711
samandari.mohammad <samandari.mohammad@gmail.com>
2019-06-17 03:26:49 +0430

    Initial commit

---
+++ app/src/main/java/ir/msamandari/droidcofe/OrderActivity.java
@@ -0,0 +1,13 @@
+package ir.msamandari.droidcofe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
